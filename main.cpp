bd866e0b91e18d4640465568ab8acae81b667e2f
anatol <anatol1988@gmail.com>
2018-05-23 18:27:34 +0300

    Parse packet type

--- main.cpp
+++ main.cpp
@@ -2,13 +2,47 @@
 #include <iostream>
 
 #include <netinet/in.h>
+#include <netinet/ip.h>
 #include <sys/socket.h>
 
 using std::array;
 using std::cin;
 using std::cout;
+using std::ostream;
 
 auto constexpr bufferSize = 0x10000;
+
+enum class PacketType { icmp = 1, igmp = 2, tcp = 6, udp = 17, other };
+
+auto getPacketType(unsigned char const* buffer) -> PacketType
+{
+    auto const* iph = reinterpret_cast<const iphdr*>(buffer);
+    auto type = PacketType{};
+    return static_cast<PacketType>(iph->protocol);
+}
+
+ostream& operator<<(ostream &out, PacketType type)
+{
+    switch (type) {
+    case PacketType::icmp:
+        out << "ICMP";
+        break;
+    case PacketType::igmp:
+        out << "IGMP";
+        break;
+    case PacketType::tcp:
+        out << "TCP";
+        break;
+    case PacketType::udp:
+        out << "UDP";
+        break;
+    default:
+        cout << "other";
+        break;
+    }
+
+    return out;
+}
 
 auto main() -> int
 {
@@ -26,7 +60,6 @@ auto main() -> int
     while (true) {
         auto saddr = sockaddr{};
         auto saddrSize = socklen_t{sizeof saddr};
-
         auto dataSize
             = recvfrom(sockRaw, &buffer, bufferSize, 0, &saddr, &saddrSize);
 
@@ -34,6 +67,7 @@ auto main() -> int
             cout << "recvfrom error, failed to get packets\n";
 
         cout << "Received bytes: " << dataSize << '\n';
+        cout << "Type: " << getPacketType(buffer.data()) << '\n';
     }
 
     cin.get();
